de1b4211d50589fee23cd2c224ef74be37babaaa
Andrija Matosic <andrija.matosic@outlook.com>
2020-10-26 22:20:52 +0100

    rijesen 3.b (treba zakomentirat jos)

--- labovi/3.zadatak/zadatak3.c
+++ labovi/3.zadatak/zadatak3.c
@@ -17,6 +17,8 @@ int unosKraj(p_osoba);    //funkcija unosi novi clan na kraj liste
 p_osoba traziPrezime(p_osoba);    //funkcija trazi clan u listi po prezimenu
 int traziUkloni(p_osoba); //funkcija trazi clan po prezimenu te ga uklanja iz liste
 int dodajIza(p_osoba);
+p_osoba traziPrethodni(p_osoba);    //funkcija trazi clan u listi po koji se nalazi ispred trazenog prezimena
+int dodajIspred(p_osoba);
 
 int main()
 {
@@ -25,7 +27,7 @@ int main()
 
     unosKraj(&head);
     ispis(head.next);
-    dodajIza(head.next);
+    dodajIspred(&head);
     ispis(head.next);
 
     return 0;
@@ -160,6 +162,59 @@ int dodajIza(p_osoba p) //argument je prvi pravi clan liste
 
     novi->next = trazeni->next;
     trazeni->next = novi;
+
+    puts("Unesi ime:");     //unos podataka
+    scanf("%s", novi->ime);
+    puts("Unesi prezime:");
+    scanf("%s", novi->prezime);
+    puts("Unesi godinu rodenja:");
+    scanf("%d", &novi->godina);
+
+    return 0;
+}
+
+p_osoba traziPrethodni(p_osoba p) //argument je adresa prvog stvarnog clan liste
+{
+    char prezime[20];   //u ovu varijablu upisujemo prezime koje trazimo
+
+    p_osoba prethodni = p;
+
+    puts("Upisi prezime koje trazite:");
+    scanf("%s", prezime);   //unosimo trazeno prezime
+
+    while (p->next != NULL && strcmp(prezime, p->prezime) != 0) //petlja se vrti dok ne dodemo do zadnjeg clana i dok vrijednost varijable prezime ne odgovara trazenom prezimenu(vidi vrh za objasnjenje strcmp();
+    {
+        prethodni = p;
+        p = p->next;    //prebacujemo se na sljedeci clan liste
+    }
+
+    if (strcmp(prezime, p->prezime) == 0)   //ako smo pronasli trazeni clan
+        printf("p_osoba je pronaden: %s\t%s\t%d\n", p->ime, p->prezime, p->godina); //ispis trazenog clana
+
+    else if (strcmp(prezime, p->prezime) != 0)  //ako trazeni clan nije pronaden
+    {
+        puts("Trazeno prezime ne postoji u listi"); //prigodna poruka
+        return NULL;
+    }
+    return prethodni;
+}
+
+int dodajIspred(p_osoba p) //argument je prvi pravi clan liste
+{
+    p_osoba novi = (p_osoba*)malloc(sizeof(_osoba));    //alociramo memoriju za novi clan
+
+    if (novi == NULL) {     //ako alokacija nije uspijel
+        return -1;
+    }
+
+    p_osoba prethodni = traziPrethodni(p);    //pronalazimo clan iza kojeg cemo dodati novi clan
+
+    if (prethodni == NULL) {  //ako clan s trazenim prezimenom nije pronaden
+        return -1;
+    }
+
+    novi->next = prethodni->next;
+    prethodni->next = novi;
 
     puts("Unesi ime:");     //unos podataka
     scanf("%s", novi->ime);

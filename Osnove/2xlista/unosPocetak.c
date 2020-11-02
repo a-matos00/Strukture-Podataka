04b68eac2bee1ec4b00c9cf9b7278f70bd18ffa5
Andrija Matosic <andrija.matosic@outlook.com>
2020-11-02 22:58:21 +0100

    2x lista unos na pocetak

--- Osnove/2xlista/unosPocetak.c
+++ Osnove/2xlista/unosPocetak.c
@@ -2,13 +2,13 @@
 
 typedef struct el* p_el;
 
-typedef struct el{
+typedef struct el {
 	int broj;
 	p_el next;
 	p_el prev;
 }_el;			//tip strukture _el
 
-int dodaj(p_el);
+int unosPocetak(p_el);
 int ispis(p_el);
 int trazi(p_el, int);
 
@@ -18,20 +18,20 @@ int main()
 	head.next = NULL;
 	head.prev = NULL;
 
-	dodaj(&head);
-	dodaj(&head);
-	dodaj(&head);
+	unosPocetak(&head);
+	unosPocetak(&head);
+	unosPocetak(&head);
 	ispis(head.next);
 	trazi(&head, 2);
 
 	return 0;
 }
 
-int dodaj(p_el p)
+int unosPocetak(p_el p)
 {
 	p_el temp;
 	p_el novi = (p_el)malloc(sizeof(struct el));
-	
+
 	if (novi == NULL)
 		return -1;
 
@@ -42,7 +42,7 @@ int dodaj(p_el p)
 		temp = p->next;
 		temp->prev = novi;
 	}
-	
+
 	novi->prev = p;
 	novi->next = p->next;
 	p->next = novi;
@@ -52,7 +52,7 @@ int dodaj(p_el p)
 
 int trazi(p_el p, int x)
 {
-	p_el prethodni, sljedeci,temp, head;
+	p_el prethodni, sljedeci, temp, head;
 
 	head = p;
 

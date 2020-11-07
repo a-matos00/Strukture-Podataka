85015a5957cd2e544427b9b862ff197ec4d31298
Andrija Matosic <andrija.matosic@outlook.com>
2020-11-07 14:54:11 +0100

    unos podataka rijesen!

--- labovi/4.zadatak/zadatak4.c
+++ labovi/4.zadatak/zadatak4.c
@@ -10,17 +10,19 @@ typedef struct clan {
 	p_clan next;
 }_clan;
 
-int izDat(FILE*, p_clan);
+int izDat(FILE*, p_clan, p_clan);
 int ispis(p_clan);
 int bufferSize(FILE*);
+int ucitajPol(char*, p_clan, p_clan);
 
 int main()
 {
 	FILE* fp;
 
-	_clan p1_HEAD;
+	_clan p1_HEAD, p2_HEAD;
 
 	p1_HEAD.next = NULL;
+	p2_HEAD.next = NULL;
 
 	fp = fopen("datoteka.txt", "r");
 
@@ -29,48 +31,36 @@ int main()
 		return -1;
 	}
 
-	izDat(fp, &p1_HEAD);
+	izDat(fp, &p1_HEAD, &p2_HEAD);
 	ispis(p1_HEAD.next);
+	puts("");
+	ispis(p2_HEAD.next);
 
 	return 0;
 }
 
-int izDat(FILE* fp, p_clan p1_HEAD)
+int izDat(FILE* fp, p_clan p1, p_clan p2)
 {
-	p_clan p1;
-	char* buffer;
-	int n = 0, exp = 0, koef = 0,r = 0;
+	int buffer_size = 0;
+	p_clan novi = NULL;
+	char* buffer = NULL;
+	int n = 0, exp = 0, koef = 0, r = 0;
 	
-	buffer = (char*)malloc(bufferSize(fp));
-	
-	fgets(buffer, strlen(buffer), fp);
+	buffer_size = bufferSize(fp);
 
-	rewind(fp);
+	buffer = (char*)calloc(buffer_size, sizeof(char));	//ALOKACIJA BUFFERA
+	fread(buffer, buffer_size,1, fp);
 
-	
-		r = sscanf(buffer, "%d %d %n", &koef, &exp, &n);
+	//printf("DULJINA BUFFERA JE %d\n", strlen(buffer)); //provjera duljine buffera
+	printf("ISPIS BUFFERA: %s\n", buffer);	//PROVJERA BUFFERA
 
-		printf("koef = %d exp = %d n = %d ", koef, exp, n);
+	n = ucitajPol(buffer, p1, novi);
+	printf("OFFSET JE %d\n", n);
 
-		if (r == EOF) {
-			puts("GRESKA U CITANJU");
-			return -1;
-		}
+	buffer = (buffer + n);
 
-		else if (r != EOF) {
-			puts("SSCANF USPJESAN");
-		}
+	ucitajPol(buffer, p2, novi);
 
-		fseek(fp, n, SEEK_SET);
-
-		p1 = (p_clan)malloc(sizeof(_clan)); //stvaramo novu strukturu
-
-		p1->koef = koef;
-		p1->exp = exp;
-
-		p1->next = p1_HEAD->next;
-		p1_HEAD->next = p1;
-	
 	rewind(fp);
 
 	return 0;
@@ -87,7 +77,7 @@ int ispis(p_clan p)
 
 	while (p != NULL)   //petlja se odvija dok ne dodemo do zadnjeg clana( zadnji clan pokazuje na "nista" jer nema svog sljedbenika unutar liste dok svi ostali imaju)
 	{
-		printf("%dx^%d", p->koef, p->exp); //ispis podataka trenutnog clana liste
+		printf("%dx^%d ", p->koef, p->exp); //ispis podataka trenutnog clana liste
 		p = p->next;    //prebacujemo se na sljedeci clan u listi
 	}
 
@@ -95,16 +85,74 @@ int ispis(p_clan p)
 }
 
 int bufferSize(FILE* fp) {
-	int n = 0;
-
-	while (!feof(fp)) {
-		++n;	//brojac znakova
-		fgetc(fp);
-	}
+	int buffer_size = 0;
+	
+	fseek(fp, 0, SEEK_END);	//stavljam file pointer na kraj
+	buffer_size = ftell(fp);	// funkcija vraca kolicinu bajtova od pocetka do trenutne pozicije
 	rewind(fp);
 
-	printf("DULJINA BUFFERA JE %d\n", n);
+	printf("DULJINA BUFFERA JE %d\n", buffer_size);
 
-	return n;
+	return buffer_size;
 }
+
+int ucitajPol(char* buffer, p_clan p, p_clan novi)
+{
+	int koef, exp, n, r;
+	int offset = 0;
+	p_clan head = p;
+
+	while (buffer != EOF) {
+		r = sscanf(buffer, "%dx^%d +%n", &koef, &exp, &n);
+
+		printf("PROCITANO koef = %d exp = %d n = %d ", koef, exp, n);
+
+		if (r == EOF) {
+			puts("GRESKA U CITANJU");
+			return -1;
+		}
+
+		if (r == 0) {	// procitan redak
+			puts("Pozvan uvjet za 0, prekid petlje");
+			break;
+		}
+
+		else if (r != EOF) {
+			puts("SSCANF USPJESAN");
+		}
+
+		offset += n;
+
+		buffer = (buffer + n);	//pomicemo pokazivac buffera na ono mjesto do kojeg smo obavili sscanf
+
+		novi = (p_clan)malloc(sizeof(_clan)); //stvaramo novu strukturu
+
+		novi->koef = koef;
+		novi->exp = exp;
+
+		if (p->next == NULL) {
+			novi->next = p->next;
+			p->next = novi;
+
+		}
+
+		else {
+			while (p->next != NULL && p->next->exp < novi->exp) {
+				p = p->next;
+			}
+
+			novi->next = p->next;
+			p->next = novi;
+		}
+		p = head;
+
+
+	}
+
+	offset -= 1;	//kod cita 1 offset viska
+
+	return offset;
+}
+
+
 

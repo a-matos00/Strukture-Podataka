9400e3661eaab99646bb85286632b78ab57cf65d
Andrija Matosic <56841259+a-matos00@users.noreply.github.com>
2020-12-12 15:09:25 +0100

    Update zadatak7.c

--- labovi/7.zadatak/zadatak7.c
+++ labovi/7.zadatak/zadatak7.c
@@ -1,26 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct el* p_el;
+#include "stog.h"
+#include "buffer.h"
 
-typedef struct el{
-	int broj;
-	p_el next;
-}_el;
+
 
 int bufferSize(FILE* fp);
 char* createBuffer(FILE*);
 int izrRezPostfix(p_el, char*);
 p_el newElement(int);
+int push(p_el, p_el);
+int ispis(p_el);
+int* operandi(p_el);
+int operacija(p_el, char);
+int pop();
+
 
 int main()
 {
 	FILE* fp;
 	_el HEAD_stog;
+	HEAD_stog.next = NULL;
+	int rezultat = 0;
 
 	char* postfix_izraz;
 
-	fp = fopen("datoteka.txt", "r");
+	fp = fopen("postfix.txt", "r");
 
 	if( fp == NULL){
 		puts("Greska u otvaranju datoteke");
@@ -29,67 +35,105 @@ int main()
 
 	postfix_izraz = createBuffer(fp);
 	
-	izrRezPostfix(&HEAD_stog, postfix_izraz);
+	rezultat = izrRezPostfix(&HEAD_stog, postfix_izraz);
 
+	printf("Rezultat postfix izraza je %d", rezultat);
 	system("pause");
 
 	return 0;
 }
 
-int izrRezPostfix(p_el p, char* str)
-{
-	int ss_return_val;
-	int n;
-	char znak;
+int* operandi(p_el p) {
 
-	ss_return_val = sscanf(str, "%d", &znak , &n);
+	int* operandi = 0;
 
-	return 0;
-}
-
-p_el newElement(int arg_br)
-{
-	p_el novi_el = (p_el)malloc(sizeof(_el));
-	novi_el->broj = arg_br;
-
-	return novi_el;
-}
-
-int push(p_el p, p_el novi)	//argument je head element
-{
-
-	if( p == NULL){
-		puts("Neispravan argument, null pokazivac");
-		return -1;
+	if (p == NULL) {
+		puts("NEISPRAVAN ARGUMENT(null pointer)!");
+		return NULL;
 	}
 
-	novi->next = p->next;
-	p->next = novi;
-
-	return 0;
-}
-
-char* createBuffer(FILE* fp)
-{
-	char* buffer;
-
-	buffer = (char*)calloc(bufferSize(fp) + 1, sizeof(char));	//ALOKACIJA BUFFERA
-	fread(buffer, bufferSize(fp), 1, fp);	//sadrzaj datoteke se upisuje u buffer
-
-	//printf("Ispis buffera datoteke %s\n", buffer);	za debug
-
-	return buffer;
-}
-
-int bufferSize(FILE* fp) {	//racuna se velicina buffera
-	int buffer_size = 0;
+	else if (p->next == NULL || p->next->next == NULL) {
+		puts("Nedovoljan broj argumenata!");
+		return NULL;
+	}
 	
-	fseek(fp, 0, SEEK_END);	//stavljam file pointer na kraj
-	buffer_size = ftell(fp);	// funkcija vraca kolicinu bajtova od pocetka do trenutne pozicije
+	operandi = (int*)malloc(sizeof(int) * 2);
 
-	//printf("Velicina buffera je %d znakova\n", buffer_size);	za debuggiranje
+	*(operandi + 0) = p->next->broj;
+	*(operandi + 1) = p->next->next->broj;
 
-	rewind(fp);	//pokazivac datoteke se vraca na pocetak
+	
+	return operandi;
 
-	return buffer_size;
 }
+
+int operacija(int* operandi, char operator)
+{
+	int a = 0, b = 0;
+
+	b = *(operandi + 0);
+	a = *(operandi + 1);
+
+	if (operandi == NULL) {
+		puts("Greska pri pronalazenju operanada");
+		return -1;	//UPITNO!!!!
+	}
+
+	switch (operator) {
+
+	case '+': return a + b; break;
+	case '*': return a * b; break;
+
+	}
+}
+
+int izrRezPostfix(p_el HEAD_stog, char* str)
+{
+	int r;
+	int read_offset = 0;
+	char operator;
+	int znak;
+	int rezultat = 0;
+
+	while (str != EOF) {
+
+		r = sscanf(str, "%d%n", &znak, &read_offset);
+
+		/* printf("READ OFFSET == %d\n", read_offset);
+		printf("RETURN VALUE === %d\n", r);
+		printf("%s\n", str);
+		*/	//ZA DEBUG
+
+		if (r == EOF) {
+			//puts("Kraj datoteke");	//debug
+			return -1;
+		}
+
+		if (r == 1) {
+			printf("Ucitan je broj %d\n", znak);
+		
+			push(HEAD_stog, newElement(znak));
+			
+			ispisStoga(HEAD_stog);
+		}
+
+		if (r == 0) {
+			operator = *(str + 1);
+			printf("Ucitan je znak %c\n", operator);
+
+			rezultat = operacija(operandi(HEAD_stog), operator);
+
+		}
+
+		str += read_offset;	//pomice se pokazivac u stringu
+	}
+
+	return rezultat;
+}
+
+
+
+
+
+
+

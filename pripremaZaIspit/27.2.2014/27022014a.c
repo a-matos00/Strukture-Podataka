23d2f7400939d6101c215ae6f39d8e08d2a6a096
andrija <andrija.matosic@outlook.com>
2021-02-10 18:12:43 +0100

    Doda mape

---
+++ pripremaZaIspit/27.2.2014/27022014a.c
@@ -0,0 +1,8 @@
+/*
+Strukture podataka – 2 ispitni rok
+27.2.2014.
+Napisati program koji iz datoteke čita dio teksta, riječ po riječ i unosi u binarno stablo pretraživanja
+(maksimalna duljina riječi 1024 znaka). Ispisati binarno stablo na „inorder“ način.
+Iz binarnog stabla kreirati dvije datoteke, u prvoj se nalaze sve riječi koje počinju samoglasnikom, a u
+drugoj sve riječi koje počinju suglasnikom. Riječi zapisane u datotekama moraju biti sortirane po abecedi.
+*/
